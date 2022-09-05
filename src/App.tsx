b5c68b1840d13a2a08b0544a0b93e05495fcc565
Nguyen Le Duy <le-duy.nguyen@ekino.com>
2022-09-05 17:58:19 +0700

    clean unUse code at app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import './App.scss'
-import { Floating } from './components/Floating'
-import { PlusIcon } from './components/Icons'
-
-const FloatingWrapper = () => {
-  return (
-    <div className="container" style={{ backgroundColor: 'lightgray' }}>
-      <Floating Icon={<PlusIcon />} />
-    </div>
-  )
-}
 
 function App() {
-  return (
-    <div className="App">
-      <FloatingWrapper />
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default App
