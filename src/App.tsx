79a0edb3b7a6ce77e79817e1edbb4f2ae23e79bd
duy-ekn <le-duy.nguyen@ekino.com>
2022-11-23 11:07:46 +0700

    smoothing container

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { useRef, useState } from 'react'
 import './App.scss'
-import { LeftSide as Wrapper } from './components'
 
 function App() {
-  const [state, setState] = useState(false)
-  const ref = useRef<HTMLDivElement>(null)
-
-  return (
-    <div className="App">
-      <Wrapper state={state}>
-        <div
-          className="containerX"
-          ref={ref}
-          onClick={() => setState((prev) => !prev)}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-          voluptatibus hic laboriosam, amet possimus magni rerum. Possimus odio
-          repellat quam libero sit repellendus, similique quisquam amet
-          veritatis, laboriosam illo aspernatur.
-        </div>
-      </Wrapper>
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default App
